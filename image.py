dd1923308aac4c0519ae641754f83362ad7b0786
fatahullah_khan <70217922+Fataahullah@users.noreply.github.com>
2022-10-08 16:15:11 +0530

    Update image.py

--- image.py
+++ image.py
@@ -1,4 +1,13 @@
 
 from PIL import Image
 import os
-directory = "
+directory = "images/"
+update_directory ="/opt/icons/"
+for filename in os.listdir(directory):
+    if filename != ".DS_Store":
+        im = Image.open(os.path.join(directory, filename))
+        im = im.rotate(-90)
+        im = im.resize((128,128))
+        im = im.convert("RGB")
+        im.save(os.path.join(output_directory, filename+".jpeg"))
+
